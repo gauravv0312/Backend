e6bd9ce49ed8f45e01b32812b2ba4a92fc267435
@gauravv0312 <70060764+gauravv0312@users.noreply.github.com>
2022-08-10 16:20:24 +0530

    added one more method to store images in multer

--- multer/index.js
+++ multer/index.js
@@ -3,6 +3,8 @@ const app = express();
 const multer = require('multer');
 const mongoose = require('mongoose');
 const imageModel = require('./image');
+var fs = require('fs');
+var path = require('path');
 const PORT = 5000;
 
 
@@ -26,30 +28,53 @@ const fileStorage = multer.diskStorage({
     },
 });
 
-const upload = multer({storage : fileStorage}).single('testImage');
+// const upload = multer({storage : fileStorage}).single('image');
+const upload = multer({storage : fileStorage})
 
 
 app.get('/ready',(req,res)=>{
     res.send('You ready to upload files')
 })
 
-app.post('/upload',(req,res)=>{
-     upload(req,res,(err)=>{
-        if(err)
-            console.log(err);
-        else{
-            const newImage =  imageModel.create({
-                name : req.body.name,
-                image: {
-                    data : req.file.filename,
-                    contentType: 'image/png',
-                },
-            }).then(()=>res.send('successfully upload'))
-             .catch((err)=>res.send(err));
-        }    
-     })
-});
+// app.post('/upload',(req,res)=>{
+//      upload(req,res,(err)=>{
+//         if(err)
+//             console.log(err);
+//         else{
+//             const newImage =  imageModel.create({
+//                 name : req.body.name,
+//                 image: {
+//                     data : req.file.filename,
+//                     contentType: 'image/png',
+//                 },
+//             }).then(()=>res.send('successfully upload'))
+//              .catch((err)=>res.send(err));
+//         }    
+//      })
+// });
+app.post('/upload', upload.single('image'), (req, res, next) => {
 
+	var obj = {
+		name: req.body.name,
+		img: {
+			data: fs.readFileSync(path.join(__dirname + '/images/' + req.file.filename)),
+			contentType: 'image/png'
+		}
+
+	}
+	imageModel.create(obj, (err, item) => {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			// item.save();
+			res.send(obj);
+
+            // converting buffer into images
+            // fs.writeFileSync('images/gaurav.jpg',obj.img.data);
+		}
+	});
+});
 
 app.listen(PORT,()=>{
     console.log(`server is running on ${PORT}`);
